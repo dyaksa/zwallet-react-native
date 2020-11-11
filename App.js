80d2eeb14bbfd3d6434d0fcc326229e5a7fd127e
dyaksa <diasnour0395@gmail.com>
2020-11-11 18:40:07 +0700

    add icon app with splash screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
-import React from "react";
+import SplashScreen from "react-native-splash-screen";
+import React, { useEffect } from "react";
 import MainNavigator from "./src/navigator";
 import { Provider } from "react-redux";
 import { PersistGate } from  "redux-persist/integration/react";
@@ -7,6 +8,11 @@ import configureStore from "./src/redux/store";
 
 const App = () => {
   const { store, persistor } = configureStore();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[]);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
